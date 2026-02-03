f363c471cf0eff32eed3eb10299d18e48bf7d725
Marta <martinha03luna@gmail.com>
2026-02-03 11:08:00 -0300

    Conclusão da Prática 07: Integração com WeatherAPI

--- app/src/main/java/com/example/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/example/weatherapp/ui/MapPage.kt
@@ -78,26 +78,26 @@ fun MapPage(
             }
         }
 
-        Marker(
-            state = MarkerState(position = recife),
-            title = "Recife",
-            snippet = "Marcador em Recife",
-            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
-        )
+        //Marker(
+          //  state = MarkerState(position = recife),
+          //  title = "Recife",
+          //  snippet = "Marcador em Recife",
+          //  icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+        //)
 
-        Marker(
-            state = MarkerState(position = caruaru),
-            title = "Caruaru",
-            snippet = "Marcador em Caruaru",
-            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
-        )
+        //Marker(
+          //  state = MarkerState(position = caruaru),
+          //  title = "Caruaru",
+          //  snippet = "Marcador em Caruaru",
+          //  icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+        //)
 
-        Marker(
-            state = MarkerState(position = joaopessoa),
-            title = "Joao Pessoa",
-            snippet = "Marcador em Joao Pessoa",
-            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
-        )
+        //Marker(
+          //  state = MarkerState(position = joaopessoa),
+          //  title = "Joao Pessoa",
+          //  snippet = "Marcador em Joao Pessoa",
+          //  icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+        //)
     }
 
 }
